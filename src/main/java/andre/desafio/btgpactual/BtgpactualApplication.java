3dc71c7e93b05f1d62bebd76e0d86191f6bbf7c0
André Machado <andrels.machado82@gmail.com>
2024-07-04 13:04:03 -0300

    Iniciando desafio BTG Pactual

---
+++ src/main/java/andre/desafio/btgpactual/BtgpactualApplication.java
@@ -0,0 +1,13 @@
+package andre.desafio.btgpactual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgpactualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtgpactualApplication.class, args);
+	}
+
+}
